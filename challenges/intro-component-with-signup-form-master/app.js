b274c9560b5e8e4de8da6d51682aaaef907d54a2
B1N4R1 <gorkfern07@gmail.com>
2021-07-29 15:10:50 +0200

    Form validation improvements
    Use forEach to loop through the inputs array instead of a while loop

--- challenges/intro-component-with-signup-form-master/app.js
+++ challenges/intro-component-with-signup-form-master/app.js
@@ -2,44 +2,32 @@ document.querySelector("#singupForm").addEventListener("submit", function(e){
 
     let inputs = document.querySelectorAll(".input-group input");
 
-    let i = 0, empty = 0, input, email;
-    while (i < inputs.length) {
-
-        input = inputs[i];
-
-        if (input.name == "email") {
-            email = input;
-        }
+    let empty = 0;
+    inputs.forEach(input => {
         
         if (input.value == "") {
-            if(empty == 0) e.preventDefault();
+            if(!empty) e.preventDefault(); empty = 1;
 
+            input.parentElement.lastElementChild.innerText = `${input.placeholder} cannot be empty`;
             input.parentElement.className = "input-group error";
 
-            empty = 1;
+        }
+        else if (input.name == "email") {
+
+            let value = input.value;
+            if (!value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)) {
+                if (!empty) e.preventDefault();
+
+                input.parentElement.lastElementChild.innerText = "Looks like this is not an email";
+                input.parentElement.className = "input-group error";
+                
+            }
+
         }
         else if (input.parentElement.className == "input-group error") {
             input.parentElement.className = "input-group";
         }
 
-        i++;
-
-    }
-
-    if (email.value != "") {
-
-        let value = email.value;
-        if (!value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)) {
-
-            if (!empty) {
-                e.preventDefault();
-            }
-
-            email.parentElement.lastElementChild.innerText = "Looks like this is not an email";
-            email.parentElement.className = "input-group error";
-            
-        }
-
-    }
+    });
 
 });
