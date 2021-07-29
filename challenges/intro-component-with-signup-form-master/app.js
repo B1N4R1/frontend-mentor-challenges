b34507316ac17b2f1aa9d02841cf702557b07bf4
B1N4R1 <gorkfern07@gmail.com>
2021-07-29 12:43:13 +0200

    Form validation

---
+++ challenges/intro-component-with-signup-form-master/app.js
@@ -0,0 +1,42 @@
+document.querySelector("#singupForm").addEventListener("submit", function(e){
+
+    let inputs = document.querySelectorAll(".input-group input");
+
+    let i = 0, empty = 0, input, email;
+    while (i < inputs.length) {
+
+        input = inputs[i];
+
+        if (input.name == "email") {
+            email = input;
+        }
+        
+        if (input.value == "") {
+            if(empty == 0) e.preventDefault();
+
+            input.parentElement.className = "input-group error";
+
+            empty = 1;
+        }
+
+        i++;
+
+    }
+
+    if (email.value != "") {
+
+        let value = email.value;
+        if (!value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)) {
+
+            if (!empty) {
+                e.preventDefault();
+            }
+
+            email.parentElement.lastElementChild.innerText = "Looks like this is not an email";
+            email.parentElement.className = "input-group error";
+            
+        }
+
+    }
+
+});
