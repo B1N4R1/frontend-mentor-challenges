f13f933fca66871656a13e02d1501fac83dc38f5
B1N4R1 <gorkfern07@gmail.com>
2021-08-13 13:51:23 +0200

    Design improvements

--- challenges/four-card-feature-section-master/app.js
+++ challenges/four-card-feature-section-master/app.js
@@ -1,17 +1,33 @@
 let resizeTimer;
 
 window.addEventListener("load", () => {
+
+    if (screen.width <= 768) {
+        document.getElementById("team-builder").classList.add("fade-right");
+        document.getElementById("karma").classList.add("fade-left");
+    }
+    else{
+        document.getElementById("team-builder").classList.add("fade-down");
+        document.getElementById("karma").classList.add("fade-up");
+    }
+
     document.getElementById("supervisor").classList.add("fade-left");
-    document.getElementById("team-builder").classList.add("fade-down");
-    document.getElementById("karma").classList.add("fade-up");
     document.getElementById("calculator").classList.add("fade-right");
 
     clearTimeout(resizeTimer);
 
     resizeTimer = setTimeout(() => {
+
+        if (screen.width <= 768) {
+            document.getElementById("team-builder").classList.remove("fade-right");
+            document.getElementById("karma").classList.remove("fade-left");
+        }
+        else{
+            document.getElementById("team-builder").classList.remove("fade-down");
+            document.getElementById("karma").classList.remove("fade-up");
+        }
+
         document.getElementById("supervisor").classList.remove("fade-left");
-        document.getElementById("team-builder").classList.remove("fade-down");
-        document.getElementById("karma").classList.remove("fade-up");
         document.getElementById("calculator").classList.remove("fade-right");
     }, 3000);
 
